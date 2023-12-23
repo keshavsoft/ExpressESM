e925d87535ba238273da2fcaf97f850f301dfea1
Keshav <nknnkumar@yahoo.com>
2023-12-23 23:49:50 +0530

    keshav

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // let CommonForWebSocketStart = require("./Projects/KWSServer/EntryFile");
 import { StartFunc as StartFuncPortListen } from "./PortListen.js";
+import { StartFunc as StartFuncKWSServer } from "./Projects/KWSServer/EntryFile.js";
+
+import { StartFunc as StartFuncKLowDb } from "./KLowDb/read.js";
 
 // require('dotenv').config();
 
@@ -21,14 +24,21 @@ var port = normalizePort(process.env.PORT || '3000');
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
-    res.json("kk")
+    res.json("kkhhhhhhh")
 });
+
+app.get('/k1', (req, res) => {
+    StartFuncKLowDb().then();
+    res.json("k1");
+});
+
+
 
 app.use('/', express.static(path.join(path.resolve(), 'public')))
 
 app.use(express.json({ limit: '100mb' }));
 
-// CommonForWebSocketStart(server);
+StartFuncKWSServer(server);
 
 function normalizePort(val) {
     var port = parseInt(val, 10);
