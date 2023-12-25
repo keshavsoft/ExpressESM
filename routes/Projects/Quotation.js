a8e59e4e3ffcfd9170c516b51c740eeaf788339a
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-12-25 17:30:41 +0530

    SREENIVAS

--- routes/Projects/Quotation.js
+++ routes/Projects/Quotation.js
@@ -2,7 +2,9 @@ import express from 'express';
 var router = express.Router();
 
 import { router as routerCustomers } from './Quotation/Customers.js';
+import { router as routerItems } from './Quotation/Items.js';
 
 router.use('/Customers', routerCustomers);
+router.use('/Items', routerItems);
 
 export { router };
