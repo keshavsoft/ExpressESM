7d3567cb23373275ed3d466577f02d70162b27df
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-12-27 19:01:57 +0530

    SREENIVAS

--- Dal/login/userDataDal.js
+++ Dal/login/userDataDal.js
@@ -1,7 +1,12 @@
 import { StartFunc as StartFuncKLowDb } from '../../KLowDb/KData/JSON/Login/UserData/ReadFile.js';
+import { StartFunc as StartFuncKLowDbWriteFile } from '../../KLowDb/KData/JSON/Login/UserData/WriteFile.js';
 
 let GetFunc = async () => {
     return await StartFuncKLowDb();
 };
 
-export { GetFunc };
+let PostFunc = ({ UserName, PassWord }) => {
+    return StartFuncKLowDbWriteFile({ UserName, PassWord });
+};
+
+export { GetFunc, PostFunc };
--- routes/login/userData.js
+++ routes/login/userData.js
@@ -2,8 +2,9 @@ import express from 'express';
 
 var router = express.Router();
 
-import { GetFunc } from '../../controllers/login/userDataController.js';
+import { GetFunc, PostFunc } from '../../controllers/login/userDataController.js';
 
 router.get('/', GetFunc);
+router.post('/', PostFunc);
 
 export { router };
