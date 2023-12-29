739974c4304b2e21e26715a13e9108daa20e498b
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-12-29 11:27:36 +0530

    SREENIVAS

--- Dal/projects/Quotation/Items.js
+++ Dal/projects/Quotation/Items.js
@@ -2,9 +2,14 @@
 import { StartFuncSync as StartFuncKLowDb } from '../../../KLowDb/KData/JSON/UserData/ItemsJsonFile/ReadFile.js';
 import { StartFuncNoSync as StartFuncWriteFile } from '../../../KLowDb/KData/JSON/UserData/ItemsJsonFile/WriteFile.js';
 import { StartFuncNoSync as StartFuncUpdateFile } from '../../../KLowDb/KData/JSON/UserData/ItemsJsonFile/UpdateFile.js';
+import { StartFuncNoSync as StartFuncPickById } from '../../../KLowDb/KData/JSON/UserData/ItemsJsonFile/PickById.js';
 
 let GetFuncDal = () => {
     return StartFuncKLowDb();
+};
+
+let GetIdFuncDal = ({ inId }) => {
+    return StartFuncPickById({ inId });
 };
 
 let PostFuncDal = ({ inDataToInsert }) => {
@@ -15,4 +20,4 @@ let PatchFuncDal = ({ UuId, inDataToInsert }) => {
     return StartFuncUpdateFile({ UuId, inDataToInsert });
 };
 
-export { GetFuncDal, PostFuncDal, PatchFuncDal };
+export { GetFuncDal, GetIdFuncDal, PostFuncDal, PatchFuncDal };
--- Repo/projects/Quotation/Items.js
+++ Repo/projects/Quotation/Items.js
@@ -1,7 +1,11 @@
-import { GetFuncDal, PostFuncDal, PatchFuncDal } from '../../../Dal/projects/Quotation/Items.js';
+import { GetFuncDal, GetIdFuncDal, PostFuncDal, PatchFuncDal } from '../../../Dal/projects/Quotation/Items.js';
 
 let GetFuncRepo = () => {
     return GetFuncDal();
+};
+
+let GetIdFuncRepo = ({ inId }) => {
+    return GetIdFuncDal({ inId });
 };
 
 let PostFuncRepo = ({ inDataToInsert }) => {
@@ -12,4 +16,4 @@ let PatchFuncRepo = ({ UuId, inDataToInsert }) => {
     return PatchFuncDal({ UuId, inDataToInsert });
 };
 
-export { GetFuncRepo, PostFuncRepo, PatchFuncRepo };
+export { GetFuncRepo, GetIdFuncRepo, PostFuncRepo, PatchFuncRepo };
